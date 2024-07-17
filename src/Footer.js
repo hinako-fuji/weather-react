b332f8c112c11b2146bc029924d5fb9ed4a014e6
hinako-fuji <117415389+hinako-fuji@users.noreply.github.com>
2024-07-17 13:01:03 +0900

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,9 @@
 export default function Footer() {
   return (
     <footer>
-      <a
-        href="https://github.com/hinako-fuji/weather-react"
-        rel="noreferrer"
-        target="_blank"
-      >
-        Open-source code
-      </a>
+     
+        Coded
+      
       &nbsp;by Hinako
     </footer>
   );
